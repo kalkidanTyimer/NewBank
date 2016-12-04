ab322c0c34bc03e9e4bc2ee9acaffaa0efdadd92
Kalkidanyim <kal.1990@yahoo.com>
2016-12-04 09:53:21 -0800

    intial commit

---
+++ Newbankapp/Account.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newbankapp
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum Typeofaccounts
+    {
+        Checking,
+        Saving,
+    }
+    public class Account
+    {
+        #region Static
+        private static int lastAccountNumber = 0;
+        #endregion
+        #region properties
+        public int AccountNumber { get;
+            private set; }
+        public string EmailAddress { get; set; }
+        public decimal Balance { get;
+            private set; }
+        public Typeofaccounts AccountType { get; set; }
+        #endregion
+        #region 
+        // consturactor doesnt have any return type & parameter//
+        public Account()
+        {
+            AccountNumber =
+                ++lastAccountNumber;
+        }
+
+        #endregion
+        #region Methods
+        ///>
+        public void Deposit(Decimal amount)
+        {
+            // if you write any data type other than void in your mehod it expect return type for example if write decimal return balance 
+           // Balance = Balance + amount; it i same with the second line
+            Balance += amount;
+        }
+        public decimal Withdraw (decimal amount)
+        {
+
+            if (amount <= Balance)
+            {
+                Balance -= amount;
+            }
+            return Balance;
+
+        }
+        #endregion
+    }
+
+}
---
+++ Newbankapp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newbankapp
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*******************");
+            Console.WriteLine("Welcome to my Bank");
+            Console.WriteLine("0. Exit");
+            Console.WriteLine("1. Create Account");
+            Console.WriteLine("2. Deposit");
+            Console.WriteLine("3. Withdraw");
+            var choice = Console.ReadLine();
+            switch (choice)
+                {
+                case "0":
+                    Console.WriteLine("Have a greate day!");
+                    return;
+                case "1":
+                    Console.WriteLine("Email Address");
+                    var emailadress = Console.ReadLine();
+                    var checkingAccount = new Account();
+                    checkingAccount.EmailAddress = "test@test.com";
+                    checkingAccount.AccountType = Typeofaccounts.Checking;
+                    checkingAccount.Deposit(1000.20M);
+                    Console.WriteLine($"Account Number/; {checkingAccount.AccountNumber},Balance: {checkingAccount.Balance:c}");
+                    break;
+              default:
+                    break;
+            }
+            
+
+            var savingsAccount = new Account();
+            savingsAccount.EmailAddress = "test2@test.com";
+            savingsAccount.AccountType = Typeofaccounts.Saving;
+            savingsAccount.Deposit(200M);
+            Console.WriteLine($"Account Number/; {savingsAccount.AccountNumber},Balance: {savingsAccount.Balance:c}");
+        }
+    }
+}
